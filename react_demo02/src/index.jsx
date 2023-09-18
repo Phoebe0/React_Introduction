3e48410e1ba00d1a42f2aeace1d5c8a49c5e17ac
Tricia <1552358695@qq.com>
2023-09-18 23:42:44 +0800

    feat: setState的一些说明

--- react_demo02/src/index.jsx
+++ react_demo02/src/index.jsx
@@ -7,6 +7,7 @@ export default class App extends Component {
   }
   // 只要在处理函数中调用setState方法后，不是直接执行，而是将setState操作放在一个React的任务队列中（同步）
   add = () => {
+    console.log('前', this.state.count)
     this.setState({
       // setState 是同步更新的，但是表现为延迟更新的状态【注意：并不是异步更新】
       // 如果多次调用setState更新状态，那么状态会合并，后面覆盖前面。（即：下面有三个count + 1,但是不会执行三次，只执行一次！）
@@ -19,6 +20,7 @@ export default class App extends Component {
     this.setState({
       count: this.state.count + 1,
     })
+    console.log('后', this.state.count)
   }
   render() {
     return (
