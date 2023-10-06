b8a3f9fec56ef85ed6de3324a08cfeb3d73fa462
Tricia <1552358695@qq.com>
2023-10-06 21:38:12 +0800

    feat: setState补充

--- react_demo02/src/index.jsx
+++ react_demo02/src/index.jsx
@@ -1,43 +1,7 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom/client'
 
-export default class App extends Component {
-  state = {
-    count: 0,
-  }
-  // 只要在处理函数中调用setState方法后，不是直接执行，而是将setState操作放在一个React的任务队列中（同步）
-  add = () => {
-    console.log('前', this.state.count)
-    this.setState({
-      count: this.state.count + 1,
-    })
-    this.setState({
-      count: this.state.count + 1,
-    })
-    this.setState({
-      count: this.state.count + 1,
-    })
-    console.log('后', this.state.count)
-    // 宏任务  17显示的是同步的
-    setTimeout(() => {
-      this.setState({
-        count: this.state.count + 1,
-      })
-      this.setState({
-        count: this.state.count + 1,
-      })
-      this.setState({
-        count: this.state.count + 1,
-      })
-      console.log('定时器：', this.state.count)
-    })
-  }
+export default class index extends Component {
   render() {
-    return (
-      <div>
-        {this.state.count} <button onClick={this.add}>加1</button>
-      </div>
-    )
+    return <div>index</div>
   }
 }
-ReactDOM.createRoot(document.querySelector('#root')).render(<App></App>)
