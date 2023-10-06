ee55ff7403ccdc2e2b40e454dfcb2a8aea639b18
Tricia <1552358695@qq.com>
2023-10-06 21:34:17 +0800

    feat:setState补充

--- react_demo02/06-setState介绍/index.jsx
+++ react_demo02/06-setState介绍/index.jsx
@@ -9,6 +9,9 @@ export default class App extends Component {
   add = () => {
     console.log('前', this.state.count)
     this.setState({
+      // setState 是同步更新的，但是表现为延迟更新的状态【注意：并不是异步更新】
+      // 如果多次调用setState更新状态，那么状态会合并，后面覆盖前面。（即：下面有三个count + 1,但是不会执行三次，只执行一次！）
+      // 所有操作都执行完毕，React拿到最终状态，然后触发组件更新。
       count: this.state.count + 1,
     })
     this.setState({
@@ -18,19 +21,6 @@ export default class App extends Component {
       count: this.state.count + 1,
     })
     console.log('后', this.state.count)
-    // 宏任务  17显示的是同步的
-    setTimeout(() => {
-      this.setState({
-        count: this.state.count + 1,
-      })
-      this.setState({
-        count: this.state.count + 1,
-      })
-      this.setState({
-        count: this.state.count + 1,
-      })
-      console.log('定时器：', this.state.count)
-    })
   }
   render() {
     return (
