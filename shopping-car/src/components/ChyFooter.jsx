fff48d0a10dc5793b93694dfabcc4a2afbedbb64
Tricia <1552358695@qq.com>
2024-01-02 21:50:34 +0800

    feat: add footer

--- shopping-car/src/components/ChyFooter.jsx
+++ shopping-car/src/components/ChyFooter.jsx
@@ -10,6 +10,15 @@ export default function ChyFooter({ prdList, changeSelectAll }) {
   // select_all ： Based on the pState of each item in prdList, decide select_all is checked or not
   // If every pState is 1,isCheckAll must be true
   const isCheckAll = prdList.every((item) => item.pState === 1)
+  // total money & total number
+  // 1. If pState is 1，get it
+  // 2. accumulate
+  const totalNum = prdList
+    .filter((item) => item.pState === 1)
+    .reduce((sum, item) => sum + item.pNum, 0)
+  const totalPrice = prdList
+    .filter((item) => item.pState === 1)
+    .reduce((sum, item) => sum + item.pPrice, 0)
 
   return (
     <div className={style.footer}>
@@ -23,12 +32,12 @@ export default function ChyFooter({ prdList, changeSelectAll }) {
       </div>
       <div className={style.total}>
         TOTAL:
-        <span>$114514</span>
+        <span className={style.pro_price}>$ {totalPrice}</span>
       </div>
       <div className={style.settle}>
         <Button type="dashed" ghost onClick={onClick}>
           SELLTE
-          <span>(5)</span>
+          <span>({totalNum})</span>
         </Button>
       </div>
     </div>
