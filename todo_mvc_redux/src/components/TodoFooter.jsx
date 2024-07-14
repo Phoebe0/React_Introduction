35e1ec5d1a5b45d2286f5af6b09574fa78b51e33
Tricia <1552358695@qq.com>
2024-07-14 23:42:08 +0800

    feat:筛选已完成

--- todo_mvc_redux/src/components/TodoFooter.jsx
+++ todo_mvc_redux/src/components/TodoFooter.jsx
@@ -1,34 +1,45 @@
 import React, { useMemo } from 'react'
-import {  useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { changeAll } from '../store/actions/todo'
 
 export default function TodoFooter() {
- const todos = useSelector((state) => state.todos)
-const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch()
   // 计算属性：正在进行中的数量、提示文字
   const { activedNum, activeTodoWord } = useMemo(() => {
-    const activedNum = todos.filter((item) =>!item.isDone).length
-    const activeTodoWord = activedNum > 1? 'items' : 'item'
+    const activedNum = todos.filter((item) => !item.isDone).length
+    const activeTodoWord = activedNum > 1 ? 'items' : 'item'
     return { activedNum, activeTodoWord }
   }, [todos])
 
   // 渲染未完成的数量
-  const actived = activedNum > 0? <span className="todo-count"><strong>{activedNum}</strong> {activeTodoWord} left</span> : <span className="todo-count"><strong>0</strong> {activeTodoWord} left</span>
-  
+  const actived =
+    activedNum > 0 ? (
+      <span className="todo-count">
+        <strong>{activedNum}</strong> {activeTodoWord} left
+      </span>
+    ) : (
+      <span className="todo-count">
+        <strong>0</strong> {activeTodoWord} left
+      </span>
+    )
+
   return (
     <footer className="footer">
-      <span className="todo-count">
-      {actived}
-      </span>
+      <span className="todo-count">{actived}</span>
       <ul className="filters">
         <li>
-            <a className="selected" href="#/" onClick={() => {
-              dispatch(changeAll(todos.every(item => item.isDone)))
-              }}>
-              All
-            </a>
+          <a
+            className="selected"
+            href="#/"
+            onClick={() => {
+              dispatch(changeAll(todos.every((item) => item.isDone)))
+            }}
+          >
+            All
+          </a>
         </li>
-        <li>
+        <li onClick={() => {}}>
           <a href="#/active">Active</a>
         </li>
         <li>
