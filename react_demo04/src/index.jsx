3d7728d4f7e9a3df711ba51cf121184ebdb1fb24
Tricia <1552358695@qq.com>
2024-07-28 23:59:27 +0800

    feat:手动实现路由

--- react_demo04/src/index.jsx
+++ react_demo04/src/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-
+// 根据hash值的变化，实现页面的切换
 export const index = () => {
-  return <div>index</div>
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="#">首页</a>
+        </li>
+        <li>
+          <a href="#">关于</a>
+        </li>
+      </ul>
+    </>
+  )
 }
