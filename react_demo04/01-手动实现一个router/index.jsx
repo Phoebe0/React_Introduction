6871ddb967901500f6883a9ae9ddbe49ee54e6ed
Tricia <1552358695@qq.com>
2024-08-01 00:15:10 +0800

    feat: react-router初体验

---
+++ react_demo04/01-手动实现一个router/index.jsx
@@ -0,0 +1,6 @@
+// 1. 导入 useRef hook
+import React, { useState, useRef } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>)
--- react_demo04/src/App.jsx
+++ react_demo04/src/App.jsx
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import Home from './views/Home'
-import About from './views/About'
-import Author from './views/Author'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
 export default function () {
-  // 模拟路由的实现：按需展示。使用window.location.hash
-  const [path, usePath] = useState(window.location.hash.slice(1))
-  // hashchange事件可监听hash值变化
-  useEffect(() => {
-    window.addEventListener('hashchange', () => {
-      // 监听事件变化之后，重新设置path值
-      usePath(window.location.hash.slice(1))
-    })
-  }, [])
   return (
-    <>
-      <ul>
-        <li>
-          <a href="#/home">首页</a>
-        </li>
-        <li>
-          <a href="#/about">关于</a>
-        </li>
-        <li>
-          <a href="#/author">我的</a>
-        </li>
-      </ul>
-      {path === '/home' && <Home></Home>}
-      {path === '/about' && <About></About>}
-      {path === '/author' && <Author></Author>}
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
+}
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
 }
