7fe482988861a78b81c8e535a151c658a302ed38
Tricia <1552358695@qq.com>
2024-10-14 22:30:13 +0800

    feat：嵌套路由认识

---
+++ react_demo04/05-嵌套路由/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from 'react-router-dom'
+import Home from './views/Home'
+import About from './views/About'
+import NotFound from './views/NotFound'
+import './css/index.css'
+export default function App() {
+  return (
+    <div>
+      <h1>App根组件</h1>
+      <Router>
+        <NavLink to="/" exact>
+          首页
+        </NavLink>
+        <NavLink to="/about" activeClassName="aboutCurrent">
+          关于
+        </NavLink>
+        <NavLink to="/404" exact>
+          404
+        </NavLink>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          {/* 匹配是从上到下匹配的，所以404页面必须放在最后。这样其他的path没有匹配到的话就展示404 */}
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
---
+++ react_demo04/05-嵌套路由/views/AboutI.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AboutI() {
+  return <div>AboutI</div>
+}
