89217e6c8e004af3284620d36b9434b9c18fbc0e
Tricia <1552358695@qq.com>
2024-10-17 00:00:13 +0800

    feat：useEffect使用ts

--- my-react-ts/src/App.tsx
+++ my-react-ts/src/App.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 
-// 1. 使用useState添加类型
-// 基本数据类型可以不传递泛型，自动推论
+// useEffect在ts环境下和js使用方式一样。不需要传泛型
 export default function App() {
-  const [num, setNum] = useState<number>(1)
+  useEffect(() => {
+    console.log('useEffect-你好呀')
+  })
+
   // num加1的函数
-  const addNum = () => {
-    setNum(num + 1)
-  }
+
   return (
     <div>
       <h1>TS项目根组件</h1>
-      <p>{num}</p>
-      <button onClick={() => addNum()}>num++</button>
     </div>
   )
 }
